4977f5f5ab2aafb6e49c9ce86ba57428d5c17396
Gasiek <kacper.b.gasior@gmail.com>
2018-12-23 22:14:29 +0100

    css and dom applied

--- src/index.js
+++ src/index.js
@@ -11,29 +11,17 @@ function getWeather() {
         })
         .then(res => {
             console.log(res);
+            document.getElementById("searchInput").style.backgroundColor = "rgb(255, 255, 255)";
+            document.getElementById("errorMessage").style.display = "none";
             displayWeather(res);
         })
         .catch(err => {
             if (err.status === 404) {
                 console.log("Błąd, żądany adres nie istnieje");
-            }
-        })
-}
-
-function getWeatherByCoords(coords) {
-    let input = document.getElementById("searchInput").value;
-    fetch(`https://samples.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}`)
-        .then(res => {
-            if (res.ok) return res.json();
-            else return Promise.reject(res);
-        })
-        .then(res => {
-            console.log(res);
-            displayWeather(res);
-        })
-        .catch(err => {
-            if (err.status === 404) {
-                console.log("Błąd, żądany adres nie istnieje");
+                document.getElementsByClassName("container")[0].style.display = "none";
+                console.log(document.getElementById("searchInput"));
+                document.getElementById("searchInput").style.backgroundColor = "rgba(255, 65, 54, 0.4)";
+                document.getElementById("errorMessage").style.display = "block";
             }
         })
 }
@@ -49,53 +37,72 @@ function displayWeather(res) {
     cityName.innerHTML = res.city.name;
     currentTemp.innerHTML = `Temperature: ${Math.round(res.list[0].main.temp)} °C`;
     currentWeather.innerHTML = `Weather: ${res.list[0].weather[0].description}`;
-    currentPressure.innerHTML = `Pressure: ${res.list[0].main.pressure} hPa`
-    currentHumidity.innerHTML = `Humidity: ${res.list[0].main.humidity} %`
+    currentPressure.innerHTML = `Pressure: ${Math.round(res.list[0].main.pressure)} hPa`;
+    currentHumidity.innerHTML = `Humidity: ${res.list[0].main.humidity} %`;
     let t;
-    let k;
+
+    document.getElementsByClassName("container")[0].style.display = "grid";
 
     switch (res.list[0].dt_txt.substr(11)) {
         case "00:00:00":
-            t =4;
-            k = 0;
+            t = 4;
             break;
         case "03:00:00":
             t = 3;
-            k = 0;
             break;
         case "06:00:00":
             t = 2;
-            k =0;
             break;
         case "09:00:00":
-            t =1;
-            k = 0;
+            t = 1;
             break;
         case "12:00:00":
             t = 0;
-            k = 0;
             break;
-            //zrobic/////////////////////////////////////////////////////////
         case "15:00:00":
             t = 7;
-            k = 5;
             break;
         case "18:00:00":
             t = 6;
-            k = 6;
             break;
         case "21:00:00":
             t = 5;
-            k = 7;
             break;
     }
 
-    for (let i = 0; i < day.length; i++) {
-        day[i].children[0].innerHTML = res.list[k].dt_txt.substr(0, 10);
-        day[i].children[1].innerHTML = `Temperature: <span>${Math.round(res.list[t].main.temp)}°C </span>`;
-        day[i].children[2].innerHTML = `/ <span>${Math.round(res.list[t+4].main.temp)}°C</span>`;
-        day[i].children[3].innerHTML = `Wind: ${Math.round(res.list[0].wind.speed)} m/s`;
-        t += 8;
-        k += 8;
+    if (t <= 4) {
+
+        if (t = 4) {
+            for (let i = 0; i < day.length - 1; i++) {
+                day[i].children[0].innerHTML = res.list[t].dt_txt.substr(0, 10);
+                day[i].children[1].innerHTML = `<span>${Math.round(res.list[t].main.temp)}°C </span>`;
+                day[i].children[2].innerHTML = ` / <span>${Math.round(res.list[t+4].main.temp)}°C</span>`;
+                day[i].children[3].innerHTML = `${Math.round(res.list[t+4].wind.speed)} m/s`;
+                t += 8;
+            }
+            day[4].children[0].innerHTML = res.list[t].dt_txt.substr(0, 10);
+            day[4].children[1].innerHTML = `<span>${Math.round(res.list[t].main.temp)}°C </span>`;
+            day[4].children[2].innerHTML = ` / <span>${Math.round(res.list[t+3].main.temp)}°C</span>`;
+            day[4].children[3].innerHTML = `${Math.round(res.list[t].wind.speed)} m/s`;
+        } else {
+            for (let i = 0; i < day.length; i++) {
+                day[i].children[0].innerHTML = res.list[t].dt_txt.substr(0, 10);
+                day[i].children[1].innerHTML = `<span>${Math.round(res.list[t].main.temp)}°C </span>`;
+                day[i].children[2].innerHTML = ` / <span>${Math.round(res.list[t+4].main.temp)}°C</span>`;
+                day[i].children[3].innerHTML = `${Math.round(res.list[t].wind.speed)} m/s`;
+                t += 8;
+            }
+        }
+    } else {
+        day[0].children[0].innerHTML = res.list[0].dt_txt.substr(0, 10);
+        day[0].children[2].innerHTML = `<span>${Math.round(res.list[t - 4].main.temp)}°C</span>`;
+        day[0].children[3].innerHTML = `${Math.round(res.list[0].wind.speed)} m/s`;
+        for (let i = 1; i < day.length; i++) {
+            day[i].children[0].innerHTML = res.list[t].dt_txt.substr(0, 10);
+            day[i].children[1].innerHTML = `<span>${Math.round(res.list[t].main.temp)}°C </span>`;
+            day[i].children[2].innerHTML = ` / <span>${Math.round(res.list[t + 4].main.temp)}°C</span>`;
+            day[i].children[3].innerHTML = `${Math.round(res.list[t].wind.speed)} m/s`;
+            t += 8;
+        }
     }
 }
